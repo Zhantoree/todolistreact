f20901d44ae9efbe1acfb9d958afb1863121bea4
Zhantoree <drug2334@gmail.com>
2023-04-26 12:34:30 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header/Header";
+import AddTodo from "./components/AddTodo/AddTodo";
+import TodoList from "./components/TodoList/TodoList";
+
+function App() {
+    return (
+        <div className="App">
+            <Header/>
+            <AddTodo/>
+            <TodoList/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/AddTodo/AddTodo.jsx
@@ -0,0 +1,5 @@
+export default function AddTodo() {
+    return(
+        <div>Form</div>
+    );
+}
